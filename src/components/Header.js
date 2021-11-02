0eb88f7442d4457258f3b11e07888b7c4e8b1a4c
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2021-11-02 22:35:30 +0300

    feat: добавить ссылки и имейл пользователся

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,27 @@
+import React from 'react';
 import logo from '../images/header-logo.svg';
+import { Route, Link } from 'react-router-dom';
 
-function Header() {
+function Header({ loggedIn , headerEmail, logout }) {
+	function handleClickLogout(e) {
+		e.preventDefault();
+		logout();
+	}
 	return (
 		<header className="header">
 			<img className="header__logo" src={logo} alt="логтип Место-Россия" />
+			<div className="header__auth">
+				{loggedIn && <p className="header__auth-email">{headerEmail}</p>}
+				<Route path="/sign-in">
+					<Link className="header__auth-button" to="/sign-up">Регистрация</Link>
+				</Route>
+				<Route path="/sign-up">
+					<Link className="header__auth-button" to="/sign-in">Войти</Link>
+				</Route>
+				<Route exact path="/">
+					<Link className="header__auth-button header__auth-button_type_authorized" to="/sign-in" onClick={handleClickLogout} >Выйти</Link>
+				</Route>
+			</div>
 		</header>
 	)
 }
